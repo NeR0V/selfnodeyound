f16b0b89c2f4ec296229eff123c37e6269640044
NeR0V <34779811+NeR0V@users.noreply.github.com>
2018-08-14 23:25:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+let selfbot = 'guildMemberAdd';
+
+client.login("");
+
+client.on('ready', () =>{
+    console.log('iniciado!')
+});
+
+client.on('guildMemberAdd', member =>{
+
+    client.users.get(member.id).send('**Venha fazer parte da nossa família da Yound Community :heart: ~~~~>https://discord.gg/TZvbXAe<~~~~**')
+});
